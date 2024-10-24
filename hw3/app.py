46e72469ba76978a688df0bd73704835244c68ac
ahmad <zafarsafi98@gmail.com>
2024-10-24 12:03:02 -0700

    docs added

--- hw3/app.py
+++ hw3/app.py
@@ -1,12 +1,13 @@
 from flask import Flask
-from controllers.index import index_route
-from controllers.view_entries import view_entries_route
-from controllers.add_entry import add_entry_route
+from .controllers.index import index_route
+from .controllers.view_entries import view_entries_route
+from .controllers.add_entry import add_entry_route
 
 # Initialize the Flask application
 app = Flask(__name__)
 
 # Register the controllers (routes)
+
 # The index route handles the home page of the app.
 app.add_url_rule('/', 'index', index_route)
 
@@ -16,7 +17,12 @@ app.add_url_rule('/view', 'view_entries', view_entries_route)
 # The add_entry route allows users to add a new song, handling both GET (display form) and POST (submit form) requests.
 app.add_url_rule('/add', 'add_entry', add_entry_route, methods=['GET', 'POST'])
 
-# Entry point for running the Flask app
 if __name__ == '__main__':
-    # The app runs on host 0.0.0.0 (accessible externally), port 5000, and debug mode is enabled.
+    """
+    Entry point for running the Flask app.
+    The app is configured to:
+    - Run on host `0.0.0.0` (accessible externally)
+    - Run on port 5000
+    - Enable debug mode (useful for development)
+    """
     app.run(host='0.0.0.0', port=5000, debug=True)
